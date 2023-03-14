d8e309273ff682d87e391e1dff0205b819839849
makshoop <makshoope@gmail.com>
2023-03-14 20:14:37 +0400

    task 1 is done

--- 1 task/index.js
+++ 1 task/index.js
@@ -1,24 +1,47 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+	let intervalId;
+	let remainingSeconds;
+
+	const updateTimer = () => {
+		if (remainingSeconds <= 0) {
+			clearInterval(intervalId);
+			return;
+		}
+
+		let hours = Math.floor(remainingSeconds / 3600);
+		let minutes = Math.floor((remainingSeconds % 3600) / 60);
+		let seconds = remainingSeconds % 60;
+
+		hours = hours.toString().padStart(2, "0");
+		minutes = minutes.toString().padStart(2, "0");
+		seconds = seconds.toString().padStart(2, "0");
+
+		timerEl.textContent = `${hours}:${minutes}:${seconds}`;
+		remainingSeconds--;
+	};
+
+	return (seconds) => {
+		clearInterval(intervalId);
+		remainingSeconds = seconds;
+		updateTimer();
+		intervalId = setInterval(updateTimer, 1000);
+	};
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+inputEl.addEventListener("input", () => {
+	inputEl.value = inputEl.value.replace(/\D/g, "");
 });
 
-buttonEl.addEventListener('click', () => {
-  const seconds = Number(inputEl.value);
+buttonEl.addEventListener("click", () => {
+	const seconds = Number(inputEl.value);
 
-  animateTimer(seconds);
+	animateTimer(seconds);
 
-  inputEl.value = '';
+	inputEl.value = "";
 });
